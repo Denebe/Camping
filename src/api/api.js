821fca7be2d56c8354322b348039de74f8a58164
denebe <twhjdfo01@daum.net>
2022-08-22 18:12:30 +0900

    [fix]proxy url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PROXY_URL = '';
+const PROXY_URL = window.location.hostname === 'localhost' ? '' : '/proxy';
 
 export const mainApi = (rows, page, setDb) => {
     const url = `${PROXY_URL}/basedList?numOfRows=${rows}&pageNo=${page}&MobileOS=ETC&MobileApp=TEST&serviceKey=${process.env.REACT_APP_API_KEY}&_type=json`;
