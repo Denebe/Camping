cf75daf6a305f52057df0a7dd28611f75574d9f1
denebe <twhjdfo01@daum.net>
2022-08-22 14:42:51 +0900

    feat:navbar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,32 @@
-import React, {useEffect} from 'react'
-import { mainApi } from '../../api/api'
-
+import React, { useEffect } from "react";
+import { useNavigate } from 'react-router-dom'
+import { mainApi } from "../../api/api";
+import Logo from "../../assets/img/camplogo.png";
 const Home = () => {
-    useEffect(() => {
-        mainApi(1, 1)
-    }, [])
-    return (
-        <div>
-            home
-        </div>
-    )
-}
+  let navigate = useNavigate();
 
-export default Home
+  useEffect(() => {
+    mainApi(1, 1);
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "#000",
+        justifyContent: "center",
+        color: "#fff",
+        padding: "10px",
+        textAlign: "center",
+        margin: "auto",
+      }}
+    >
+      <div onClick={() => navigate('/')}>
+        <h1>우당탕탕 동완이와 함께하는</h1>
+        <img style={{ height: "100px" }} src={Logo} alt="Logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
